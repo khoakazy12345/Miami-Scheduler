7ebe0d1c44b9f1657a0e79420df342dfca925a03
Nguyen Khoa <ynguyenk7@miamioh.edu>
2020-09-25 10:12:16 -0400

    First Algo Commit

--- Info_Process.py
+++ Info_Process.py
@@ -4,25 +4,51 @@ text = text_file.readlines()
 for line in text:
     line = line.split()
     line = " ".join(line[1:])
-    origin = re.sub(r"[,;]"," and ",line)
+    origin = re.sub(r"[,;]", " and ", line)
     origin = origin.split()
     origin_copy = list(origin)
+    # Remove any unnecessary strings
     for string in origin_copy:
         if (re.search(r"^.?and.?$|^.?or.?$|[A-Z]{3}|[0-9]{3}", string) is None):
             origin.remove(string)
-    reverse_origin = origin[::-1]
-    for string in reverse_origin:
-        if (re.search(r"and|or", string)) is not None:
-            origin = origin[:-1]
-        else:
-            break
     origin_copy = list(origin)
-    for order in range(len(origin_copy)-1,-1,-1):
+    # get rid of graduate courses
+    for order in range(len(origin_copy)-1, -1, -1):
         if re.search(r"[5-9]{1}[0-9]{2}", origin_copy[order]) is not None:
             origin.pop(order)
-    for order in range (len(origin)):
+    # get rid of / symbol and trimming down course code + course number
+    for order in range(len(origin)):
         origin[order] = re.sub(r"\(|\)", "", origin[order])
         if re.search(r"/", origin[order]) is not None:
             origin[order] = origin[order][:3]
-    print(origin)
-    print(origin)
+    # trimming down excess "and" and "or" terms
+    i = 0
+    while i < len(origin):
+        if (origin[i-1] == "or" or origin[i-1] == "and") and (re.search(r"[0-9]{3}", origin[i]) is not None):
+            origin.insert(i, "CPB")
+            i = i + 1
+            continue
+        if re.search(r"[A-Z]{3}|[0-9]{3}", origin[i]) is not None:
+            i = i + 1
+        elif ((origin[i] == "or" or origin[i] == "and") and i == len(origin)-1):
+            origin.pop(i)
+        elif origin[i] == "or" and re.search(r"[A-Z]{3}|[0-9]{3}", origin[i+1]) is None:
+            origin.pop(i)
+        elif origin[i] == "and" and re.search(r"[A-Z]{3}|[0-9]{3}", origin[i+1]) is None:
+            origin.pop(i)
+        else:
+            i = i + 1
+    fin_list = [[]]
+    i = 0
+    while len(origin) > 0:
+        if re.search(r"[A-Z]{3}", origin[0]) is not None:
+            fin_list[i].append(" ". join(origin[0:2]))
+            origin.pop(0)
+            origin.pop(0)
+        elif origin[0] == "or":
+            origin.pop(0)
+        elif origin[0] == "and":
+            origin.pop(0)
+            fin_list.append([])
+            i = i + 1
+    print(fin_list)
--- Test.py
+++ Test.py
@@ -1,13 +1,14 @@
-import re
-text_file = open("Sample_Data.txt")
-text = text_file.readlines()
-for line in text:
-    line = line.split()
-    line = " ".join(line[1:])
-    origin = re.sub(r"[,;]"," and ",line)
-    origin = origin.split()
-    origin_copy = list(origin)
-    for i in list(origin):
-        if (re.search(r"^.?and.?$|^.?or.?$|[A-Z]{3}|[0-9]{3}", i) is None):
-            origin.remove(i)
-    print(" ".join(origin))
+fin_list = [[]]
+ i = 0
+  while len(origin) > 0:
+       if re.search(r"[A-Z]{3}", origin[i]) is not None:
+            fin_list[i].append(" ". join(origin[0:2]))
+            origin.pop(0)
+            origin.pop(0)
+        elif origin[i] == "or":
+            origin.pop(0)
+        elif origin[i] == "and":
+            origin.pop(0)
+            fin_list.append([])
+            i = i + 1
+    #print(fin_list)
